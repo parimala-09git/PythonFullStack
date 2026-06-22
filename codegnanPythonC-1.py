b0b756a3478e4d6f7a0ca4391f591cbd3dec3399
parimala-09git <parimalapathivada@gmail.com>
2026-06-22 19:14:41 +0530

    Add files via upload
    - Added overview of procedural and OOP concepts
    - Added Python features and applications
    - Included tokens and variable naming rules
    - Added basic examples for data types and memory IDs"

---
+++ codegnanPythonC-1.py
@@ -0,0 +1,119 @@
+
+#PYTHON-class1:
+
+'''
+Procedural language
+-------------------
+--->This follows a step-by-step approach, where code is structured into procedure such as function or routines...
+
+eg --> c
+
+Object oriented programming language
+------------------------------------
+--->This is based on concept of object and class...eg:(python and java)
+
+
+Python
+------
+1.dynamically typed
+-------------------
+--->Python knows the type of data we are passing to the variable...
+
+2.interpreter
+-------------
+--->Python execute code line-by-line, if any error occurs it will stop the execution in that and code which does not have any error till there that will give the output...
+
+
+eg:
+print(89+67)
+num=89
+num_2=67
+total_=num+num_2
+print(id(num))
+print(id(num_2))
+print(id(total))
+
+3.High_level
+------------
+1991 --> Guide Van Russum
+
+why use python
+--------------
+-->easy-syntax
+-->cross-platform
+-->wide application
+-->Huge library support(numpy pandas, matplotlib)
+
+
+Tokens
+------
+1)keywords 
+----------
+-->These are the reserved words in Python...
+eg:
+if, else, for, while, break, continue, pass
+
+2)Identifiers
+-------------
+-->Names given to the variables, functions, class
+
+3)Literals
+----------
+-->89, "hello", 4.56(right side of the variables)
+
+4)Operators
+-----------
+--> +, -, *, /
+
+5)Punctuations
+--------------
+--> (),[],{}
+
+
+
+Rules of variables
+------------------
+A-Z, a-z, _
+
+eg: can start with
+num_1=7
+num=8
+Num_2=9
+
+eg:
+special character, number, space
+$num=0
+su@m=8
+s o=89
+2num=6
+
+
+Comments
+--------
+We have two types of comments: single-line and multi-line...
+1)single line --> # 
+2)multi-line --> ''' ''', """ """
+
+1)single line: #
+
+num=9
+if num % 2 == 0:  #checking a number is even or odd
+   print("Even")
+else:
+   print("odd")
+
+2)multi-line: ''' '''
+It is used when we write a paragraph 
+
+
+Boolean
+-------
+
+1)True
+2)False
+
+num=8
+num_2=89
+print(num==num_2)
+
+'''
